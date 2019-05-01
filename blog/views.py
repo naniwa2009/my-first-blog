e2adc2c0cf5b0036c44d908fa9a60a1026a1c535
naniwa <naniwa2009@hotmail.co.jp>
2019-05-01 23:51:51 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    context = {
+        'abc': 123,
+        'def': 456,
+
+    }
+    return render(request, 'blog/post_list.html', context)
